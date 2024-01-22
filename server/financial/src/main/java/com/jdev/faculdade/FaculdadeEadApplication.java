0c28f927605ca37451b886585a4984d3116e1dee
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-01-22 15:13:33 -0300

    creating basic microservices

---
+++ server/financial/src/main/java/com/jdev/faculdade/FaculdadeEadApplication.java
@@ -0,0 +1,13 @@
+package com.jdev.faculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaculdadeEadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaculdadeEadApplication.class, args);
+	}
+
+}
---
+++ server/student/src/test/java/com/jdev/faculdade/FaculdadeEadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdev.faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaculdadeEadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
