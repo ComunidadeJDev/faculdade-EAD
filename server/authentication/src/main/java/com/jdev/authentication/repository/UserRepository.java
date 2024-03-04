db75924ffe5351a048899f623d922592b4552f15
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-03-04 11:28:05 -0300

    create new microservice authentication

---
+++ server/authentication/src/main/java/com/jdev/authentication/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.jdev.authentication.repository;
+
+import com.jdev.authentication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
